96e30d41dc8a8fed5766b4ca897f952b19cfa27f
Bharat Choudhary <bharatchoutharyb@gmail.com>
2026-04-14 20:23:44 +0530

    done

---
+++ Notes/howToCode.md
@@ -0,0 +1,6 @@
+
+# how to coding by yourself
+1. understanding / Feature to implement
+2. Break project Multiple small task 
+3. Now implement that most easy part first
+4. expand 
--- class-146/Backend/src/config/config.js
+++ class-146/Backend/src/config/config.js
@@ -18,6 +18,11 @@ if (!process.env.GOOGLE_CLIENT_SECRET) {
     throw new Error(" GOOGLE_CLIENT_SECRET is not defined in the environment variable");
 }
 
+if (!process.env.IMAGEKIT_PRIVATE_KEY) {
+    throw new Error(" IMAGEKIT_PRIVATE_KEY is not defined in the environment variable");
+}   
+
+
 
 
 
@@ -26,5 +31,8 @@ export const config = {
     JWT_SECRET: process.env.JWT_SECRET,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    NODE_ENV: process.env.NODE_ENV || "development",  
+    NODE_ENV: process.env.NODE_ENV || "development", 
+    IMAGEKIT_PRIVATE_KEY: process.env.IMAGEKIT_PRIVATE_KEY,
+
+
 }
